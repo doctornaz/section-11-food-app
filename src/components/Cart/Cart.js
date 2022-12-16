babc7d5561c88806740b0de09ef8f23ed37b6801
Jorge Sarmiento <doctor.n4z@gmail.com>
2022-12-16 16:09:51 -0300

    cart fixes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,24 +1,25 @@
 import React from "react"
+import Modal from "../UI/Modal";
 import { ActionsSection, CloseButton, OrderButton, TotalSection } from "./Cart.styles";
 
 
 const Cart = (props) => {
     const cartItems = [
-        { id: "c1", name: 'Sushi', amount: 2, price:12.99 },
+      { id: "c1", name: 'Sushi', amount: 2, price:12.99 },
     ].map((item)=> <li>{item.name}</li>);
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <TotalSection>
         <span>Total Amount</span>
         <span>35.62</span>
       </TotalSection>
       <ActionsSection>
-        <CloseButton>Close </CloseButton>
-        <OrderButton>Order </OrderButton>
+        <CloseButton>Close</CloseButton>
+        <OrderButton>Order</OrderButton>
       </ActionsSection>
-    </div>
+    </Modal>
   )
 };
 

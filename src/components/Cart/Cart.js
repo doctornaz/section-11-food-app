77c9278468caa90254268af684c4ad5a15c2037a
Jorge Sarmiento <doctor.n4z@gmail.com>
2022-12-16 15:42:27 -0300

    cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { ActionsSection, CloseButton, OrderButton, TotalSection } from "./Cart.styles";
+
+
+const Cart = (props) => {
+    const cartItems = [
+        { id: "c1", name: 'Sushi', amount: 2, price:12.99 },
+    ].map((item)=> <li>{item.name}</li>);
+
+  return (
+    <div>
+      {cartItems}
+      <TotalSection>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </TotalSection>
+      <ActionsSection>
+        <CloseButton>Close </CloseButton>
+        <OrderButton>Order </OrderButton>
+      </ActionsSection>
+    </div>
+  )
+};
+
+export default Cart;

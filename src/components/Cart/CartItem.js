cafef94b306132f1ad950c3928a4c920976b3d6a
Jorge Sarmiento <doctor.n4z@gmail.com>
2022-12-22 16:09:34 -0300

    145 CartItem

---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,24 @@
+import classes from './CartItem.module.css';
+import { CartItemActions, CartItemLi, CartItemPrice, CartItemSummary } from './CartItem.styles';
+
+const CartItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <CartItemLi>
+      <div>
+        <h2>{props.name}</h2>
+        <CartItemSummary>
+          <CartItemPrice>{price}</CartItemPrice>
+          <CartItemAmount>x {props.amount}</CartItemAmount>
+        </CartItemSummary>
+      </div>
+      <CartItemActions>
+        <button onClick={props.onRemove}>−</button>
+        <button onClick={props.onAdd}>+</button>
+      </CartItemActions>
+    </CartItemLi>
+  );
+};
+
+export default CartItem;
